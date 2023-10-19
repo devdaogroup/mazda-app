7717461d80f73ecc6905a7890783fef002a61156
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-10-19 16:11:19 +0330

    add sample files & config project

--- next.config.js
+++ next.config.js
@@ -1,14 +1,40 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/manifest.json$/],
+    disable: !isProd,
+});
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
-  trailingSlash: true,
-  output: "export",
-  distDir: 'out',
-  images: {
-    unoptimized: true,
-  },
+    reactStrictMode: true,
+    compress: true,
+    trailingSlash: true,
+    output: 'standalone',
+    distDir: 'out',
+    images: {
+        unoptimized: true,
+        domains: [''],
+        minimumCacheTTL: 60,
+    },
+}
+if (isProd) {
+  // Modify Webpack configuration for production
+  nextConfig.webpack = (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      };
+    }
+    return config;
+  };
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Providers} from '@/store/Provider'
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+NProgress.configure({easing: "ease", speed: 2000});
+NProgress.configure({showSpinner: true});
+import PageTransitionAnimations from "@/components/extras/PageTransitionAnimations";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <PageTransitionAnimations><Providers><Component {...pageProps} /></Providers></PageTransitionAnimations>
 }
