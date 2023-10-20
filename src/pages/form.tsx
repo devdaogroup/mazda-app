017c9d809767d490348a3933ace38df44c5b811e
Mohseni <mohsensalehi2001@gmail.com>
2023-10-20 13:07:48 +0330

    create dynamic form

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,57 +1,30 @@
 import React from 'react';
-import DynamicInput from "@/components/extras/DynamicInput";
-import {InputProps} from "@/types/FormTypes";
+import { useForm } from 'react-hook-form';
 
-
-const Form = () => {
-    const inputProps: InputProps[] = [
-        {
-            name: 'field1',
-            label: 'Field 1',
-            type: 'text',
-            regex: /^[A-Za-z0-9]+$/,
-            regexErrorMessage: 'Only letters and numbers are allowed',
-        },
-        {
-            name: 'country',
-            label: 'Select a Country',
-            type: 'dropdown',
-            options: [
-                {value: 'us', label: 'United States'},
-                {value: 'ca', label: 'Canada'},
-            ],
-        },
-        {
-            name: 'gender',
-            label: 'Select Gender',
-            type: 'radio',
-            options: [
-                {value: 'male', label: 'Male'},
-                {value: 'female', label: 'Female'},
-            ],
-        },
-        {
-            name: 'fruits',
-            label: 'Select Fruits',
-            type: 'checkbox',
-            options: [
-                {value: 'apple', label: 'Apple'},
-                {value: 'banana', label: 'Banana'},
-                {value: 'orange', label: 'Orange'},
-            ],
-        },
-    ];
-    const handleFormSubmit = (data: Record<string, any>) => {
-        console.log('Submitted Data:', data);
-        // You can perform actions with the data here
-    };
-
+export default function App() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
 
     return (
-        <div>
-            <DynamicInput inputProps={inputProps} onSubmitCallback={handleFormSubmit}/>
-        </div>
-    );
-};
+        <form className="bg-gray-500 p-4 rounded-xl w-[400px] h-[400px] flex flex-wrap border" onSubmit={handleSubmit(onSubmit)}>
+            <input className="border p-2 rounded-xl w-full text-black flex" type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
+            <input className="border p-2 rounded-xl w-full text-black flex" type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
+            <input className="border p-2 rounded-xl w-full text-black flex" type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+            <input className="border p-2 rounded-xl w-full text-black flex" type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
+            <select className="border bg-black p-2 rounded-xl w-full flex" {...register("Title", { required: true })}>
+                <option value="Mr">Mr</option>
+                <option value="Mrs">Mrs</option>
+                <option value="Miss">Miss</option>
+                <option value="Dr">Dr</option>
+            </select>
 
-export default Form;
+             <div className="w-full flex justify-start">
+                 <input className="border p-2 rounded-xl w-full flex" {...register("Developer", { required: true })} type="radio" value="Yes" />
+                 <input className="border p-2 rounded-xl w-full flex" {...register("Developer", { required: true })} type="radio" value="No" />
+             </div>
+
+            <input className="border p-2 rounded-xl w-full flex" type="submit" />
+        </form>
+    );
+}
