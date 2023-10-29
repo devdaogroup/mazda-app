3ec92eb14d30cd5c9ca3eece12f9cee750178906
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-10-29 09:56:59 +0330

    add sample swiper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,23 @@
-import HeroLayout from "@/components/sections/HeroSection";
+import CarVideos from "@/components/sections/CarVideoSection";
+import HeroSection from "@/components/sections/HeroSection";
 import HowToSection from "@/components/sections/HowToSection";
 import PrevWinner from "@/components/sections/PrevWinner";
-import CarVideos from "@/components/sections/CarVideoSection";
+import SwiperSlider from "@/plugin/SwiperSlider";
 
 function App() {
-    return (
-        <div>
-            <HeroLayout/>
-            <HowToSection/>
-            <PrevWinner/>
-            <CarVideos/>
-            {/* <FormGenerator
+  return (
+    <div>
+      <HeroSection />
+      <HowToSection />
+      <PrevWinner />
+      <CarVideos />
+      <SwiperSlider />
+      {/* <FormGenerator
         inputConfigs={inputConfigs}
         callBackFn={(data) => console.log(data)}
       /> */}
-        </div>
-    );
+    </div>
+  );
 }
 
 export default App;
