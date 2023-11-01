b05dd21449c3996e8918ae2842b310466727695f
Mohammad Nazari <88213502+Mohammad-HGH@users.noreply.github.com>
2023-11-01 15:39:41 +0330

    separate Home view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,14 @@
-import AboutUs from "@/components/sections/AboutUs/Index";
-import CarVideos from "@/components/sections/CarVideoSection";
-import HeroSection from "@/components/sections/HeroSection";
-import HowToSection from "@/components/sections/HowToSection";
-import PrevWinner from "@/components/sections/PrevWinner";
-import Touch from "@/components/sections/Touch";
-import React from "react";
+import HomeView from "@/views/HomeView";
+import Head from "next/head";
 
 function App() {
   return (
-    <React.Fragment>
-      <HeroSection />
-      <HowToSection />
-      <PrevWinner />
-      <CarVideos />
-      <AboutUs />
-      <Touch />
-    </React.Fragment>
+    <>
+      <Head>
+        <title>Landing page</title>
+      </Head>
+      <HomeView />
+    </>
   );
 }
 
